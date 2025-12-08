8606dd52ea012d083d6ea10f8d080fc15993ff29
Basil502 <basilareeckal502@gmail.com>
2025-12-08 10:37:49 +0530

    first commmit

---
+++ MuzPayroll/src/main/java/com/example/MuzPayroll/MuzPayrollApplication.java
@@ -0,0 +1,13 @@
+package com.example.MuzPayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuzPayrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuzPayrollApplication.class, args);
+	}
+
+}
---
+++ MuzPayroll/src/test/java/com/example/MuzPayroll/MuzPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MuzPayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuzPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
