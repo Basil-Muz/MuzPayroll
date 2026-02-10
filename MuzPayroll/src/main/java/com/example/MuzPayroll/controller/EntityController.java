2a9a623ee480d5331222449db348f8bacfb61d0d
Basil502 <basilareeckal502@gmail.com>
2026-02-10 12:58:47 +0530

    fetch Entity

---
+++ MuzPayroll/src/main/java/com/example/MuzPayroll/controller/EntityController.java
@@ -0,0 +1,44 @@
+package com.example.MuzPayroll.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.MuzPayroll.entity.DTO.UserEntityDTO;
+import com.example.MuzPayroll.service.EntityService;
+
+@RestController
+@RequestMapping("/entity")
+@CrossOrigin(origins = "*")
+public class EntityController {
+
+    @Autowired
+    private EntityService entityService;
+
+    @GetMapping("/fetchCompany")
+    public List<UserEntityDTO> getUserCompany(
+            @RequestParam Integer userId) {
+
+        return entityService.getUserEntities(userId, 13);
+    }
+
+    @GetMapping("/fetchBranch")
+    public List<UserEntityDTO> getUserBranch(
+            @RequestParam Integer userId) {
+
+        return entityService.getUserEntities(userId, 14);
+    }
+
+    @GetMapping("/fetchLocation")
+    public List<UserEntityDTO> getUserLocation(
+            @RequestParam Integer userId) {
+
+        return entityService.getUserEntities(userId, 15);
+    }
+
+}
